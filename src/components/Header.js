6757c358c3a4a96eef1247be6b152aa50f5554e9
amodeose <amodeosa@gmail.com>
2022-09-05 18:49:28 -0400

    add cover image to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,15 @@
 import classes from "./Header.module.css";
 
-const Header = props => {
-    return (
-        <header className={classes.header}>
-            <h1>React Kitchen</h1>
-            <h3>Cart</h3>
-        </header>
-    )
+const Header = (props) => {
+  return (
+    <header className={classes.header}>
+      <div>
+        <h1>React Kitchen</h1>
+        <h3>Cart</h3>
+      </div>
+      <img src={require("../assets/wallpaper.jpg")} alt="food wallpaper"/>
+    </header>
+  );
 };
 
 export default Header;
---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+import classes from "./Main.module.css";
+
+const Main = props => {
+    return (
+        <main>
+            
+        </main>
+    )
+};
+
+export default Main;
