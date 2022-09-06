2f8d130dccc1061a9c01bdc6737f1acacd1822d8
amodeose <amodeosa@gmail.com>
2022-09-05 20:29:41 -0400

    add dummy data

--- src/components/ItemDisplay.js
+++ src/components/ItemDisplay.js
@@ -1,7 +1,10 @@
 import classes from "./ItemDisplay.module.css";
+import items from "../assets/items";
 
 const ItemDisplay = props => {
-
+    return <div className={classes.display}>
+    
+    </div>
 };
 
 export default ItemDisplay;
