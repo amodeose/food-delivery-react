003db0de21e836672d2cd22a71f23a7a101103ad
amodeose <amodeosa@gmail.com>
2022-09-05 20:30:09 -0400

    add dummy data

---
+++ src/assets/items.js
@@ -0,0 +1,18 @@
+const items = {
+  pasta: [
+    {
+      id: "p1",
+      title: "Vegetable Lasagna",
+      img: "https://images.pexels.com/photos/4079520/pexels-photo-4079520.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      price: 15.99,
+    },
+    {
+        id: "p2",
+        title: "Fettuccine Alfredo",
+        img: "https://images.pexels.com/photos/11220208/pexels-photo-11220208.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        price: 14.99
+    }
+  ],
+};
+
+export default items;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,14 @@
 import classes from "./Header.module.css";
 import coverPhoto from "../assets/wallpaper.jpg";
+import Button from "./UI/Button";
+import CartIcon from "./CartIcon";
 
 const Header = (props) => {
   return (
     <header className={classes.header}>
       <div>
         <h1>React Kitchen</h1>
-        <h3>Cart</h3>
+        <Button>Cart <CartIcon /> </Button>
       </div>
       <img src={coverPhoto} alt="table with food and drinks" />
     </header>
