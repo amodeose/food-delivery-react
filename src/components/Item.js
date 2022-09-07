88877d781cced5986e4b2c95cb11d94a5d503379
amodeose <amodeosa@gmail.com>
2022-09-06 22:04:31 -0400

    render quantity to each item card

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,9 +1,17 @@
 import classes from "./Item.module.css";
 import Button from "./UI/Button";
-import { useDispatch } from "react-redux/es/exports";
+import { useDispatch, useSelector } from "react-redux/es/exports";
 
 const Item = (props) => {
   const dispatch = useDispatch();
+  const items = useSelector((state) => state.items);
+
+  const existingItemIndex = items.findIndex(
+    (item) => item.title === props.title
+  );
+  const existingItem = items[existingItemIndex];
+
+  const currentAmount = existingItem ? existingItem.amount : 0;
 
   const currentItem = {
     title: props.title,
@@ -23,6 +31,7 @@ const Item = (props) => {
       <img src={props.img} />
       <h3>{props.title}</h3>
       <p>${props.price}</p>
+      <p className={classes.currentAmount}>Quantity: {currentAmount}</p>
       <div className={classes.itemControls}>
         <Button onClick={addItem}>Add</Button>
         <Button onClick={removeItem}>Remove</Button>
