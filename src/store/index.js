a07c55a9ca0e5202c5efcb56bebd1cdfab16e35b
amodeose <amodeosa@gmail.com>
2022-09-06 21:35:54 -0400

    add item logic

--- src/store/index.js
+++ src/store/index.js
@@ -1,31 +1,56 @@
-import { createStore } from 'redux';
+import { createStore } from "redux";
 
 const defaultCartState = {
-    items: [],
-    totalPrice: 0,
-    totalItems: 0
+  items: [],
+  totalPrice: 0,
+  totalItems: 0,
 };
 
 const cartReducer = (state = defaultCartState, action) => {
-    if (action.type === 'add') {
-        console.log('adding item');
-        return {
-            items: state.items.concat(action.payload),
-            totalPrice: state.totalPrice,
-            totalItems: state.totalItems + 1
-        };
+  if (action.type === "add") {
+    //total price and items can be updated without checking anything
+    const updatedTotalPrice = state.totalPrice + action.payload.price;
+    const updatedTotalItems = state.totalItems + 1;
+
+    //look for existing item in cart and assign it to existingCartItem
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.title === action.payload.title
+    );
+    const existingCartItem = state.items[existingCartItemIndex];
+
+    let updatedItems;
+
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + 1,
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems = state.items.concat({
+        title: action.payload.title,
+        price: action.payload.price,
+        amount: 1
+      });
     }
 
-    if (action.type === 'remove') {
-        console.log('removing item');
-        return {
-            items: state.items,
-            totalPrice: state.totalPrice,
-            totalItems: state.totalItems - 1
-        };
-    }
+    return {
+      items: updatedItems,
+      totalPrice: updatedTotalPrice,
+      totalItems: updatedTotalItems,
+    };
+  }
 
-    return state;
+  if (action.type === "remove") {
+    return {
+      items: state.items,
+      totalPrice: state.totalPrice,
+      totalItems: state.totalItems - 1,
+    };
+  }
+
+  return state;
 };
 
 const store = createStore(cartReducer);
