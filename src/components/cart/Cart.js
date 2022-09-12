723106c5df2f1be309709357762a3af0eb884472
amodeose <amodeosa@gmail.com>
2022-09-12 17:29:07 -0400

    move item render logic

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,49 +1,32 @@
 import classes from "./Cart.module.css";
 import Modal from "../UI/Modal";
-import CartItem from "./CartItem";
 import Button from "../UI/Button";
 import OrderForm from "./OrderForm";
 import { cartActions } from "../../store";
 import { useSelector, useDispatch } from "react-redux";
 import { useState } from "react";
+import CartItems from "./CartItems";
 
 const Cart = (props) => {
   const [showForm, setShowForm] = useState(false);
   const dispatch = useDispatch();
-  const totalPrice = useSelector((state) => state.totalPrice);
-  const items = useSelector((state) => state.items);
 
   const handleOrder = () => {
     setShowForm(true);
-  }
-
-  const handleClose = () => {
-    props.onClose();
-  }
+  };
 
   const handleClear = () => {
     dispatch(cartActions.clear());
-  }
+  };
 
-  if (items.length === 0) {
-    handleClose();
-  }
-
-  const cartItems = items.map((item) => (
-    <CartItem
-      key={item.title}
-      price={item.price}
-      title={item.title}
-      amount={item.amount}
-    />
-  ));
+  const handleClose = () => {
+    props.onClose();
+  };
 
   return (
     <Modal onClose={props.onClose}>
       <div className={classes.cartContent}>
-        <h1>Cart</h1>
-        <div className={classes.cartItems}>{cartItems}</div>
-        <h1>Total Price: ${totalPrice.toFixed(2)}</h1>
+        <CartItems onClose={props.onClose} />
         {showForm && <OrderForm />}
         <div className={classes.cartControls}>
           <Button onClick={handleClear}>Clear Cart</Button>
--- src/components/cart/CartItems.js
+++ src/components/cart/CartItems.js
@@ -1,7 +1,38 @@
 import classes from "./CartItems.module.css";
+import CartItem from "./CartItem";
+import Button from "../UI/Button";
+import { cartActions } from "../../store";
+import { useSelector, useDispatch } from "react-redux";
 
-const CartItems = props => {
+const CartItems = (props) => {
+  const dispatch = useDispatch();
+  const totalPrice = useSelector((state) => state.totalPrice);
+  const items = useSelector((state) => state.items);
 
+  const handleClose = () => {
+    props.onClose();
+  };
+
+  if (items.length === 0) {
+    handleClose();
+  }
+
+  const cartItems = items.map((item) => (
+    <CartItem
+      key={item.title}
+      price={item.price}
+      title={item.title}
+      amount={item.amount}
+    />
+  ));
+
+  return (
+    <div>
+      <h1>Cart</h1>
+      <div className={classes.cartItems}>{cartItems}</div>
+      <h1>Total Price: ${totalPrice.toFixed(2)}</h1>
+    </div>
+  );
 };
 
 export default CartItems;
