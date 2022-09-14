96e5d390b88b06804cffc74982ed670051b87545
amodeose <amodeosa@gmail.com>
2022-09-13 21:46:08 -0400

    input validation and submission message

--- src/components/cart/OrderForm.js
+++ src/components/cart/OrderForm.js
@@ -10,25 +10,56 @@ const OrderForm = (props) => {
   const addressRef = useRef();
   const postalCodeRef = useRef();
 
-  const submitHandler = event => {
+  const submitHandler = (event) => {
     event.preventDefault();
+    const firstName = firstNameRef.current.value.trim()
+    const lastName = lastNameRef.current.value.trim()
+    const address = addressRef.current.value.trim()
+    const postalCode = postalCodeRef.current.value.trim()
+    if (firstName && lastName && address && postalCode) {
+      setSubmitted(true);
+    };
 
+    if (!firstName) {
+      firstNameRef.current.className = classes.invalid;
+    };
+    if (!lastName) {
+      lastNameRef.current.className = classes.invalid;
+    };
+    if (!address) {
+      addressRef.current.className = classes.invalid;
+    };
+    if (!postalCode) {
+      postalCodeRef.current.className = classes.invalid;
+    };
+  };
+
+  const changeHandler = event => {
+    event.target.className = "";
+  }
+
+  if (submitted) {
+    return (
+      <div>
+        <h1>Order Submitted</h1>
+      </div>
+    );
   }
 
   return (
     <form onSubmit={submitHandler} className={classes.orderForm}>
       <h1>Details</h1>
       <label>First Name</label>
-      <input ref={firstNameRef}/>
+      <input onChange={changeHandler} ref={firstNameRef} />
       <label>Last Name</label>
-      <input ref={lastNameRef}/>
+      <input onChange={changeHandler} ref={lastNameRef} />
       <label>Address</label>
-      <input ref={addressRef}/>
+      <input onChange={changeHandler} ref={addressRef} />
       <label>Postal Code</label>
-      <input ref={postalCodeRef}/>
+      <input onChange={changeHandler} ref={postalCodeRef} />
       <div className={classes.formControls}>
         <Button onClick={props.handleCancel}>Cancel</Button>
-        <Button type='submit'>Submit</Button>
+        <Button type="submit">Submit</Button>
       </div>
     </form>
   );
