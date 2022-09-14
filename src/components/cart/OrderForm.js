2d92ffe39f586df3e4b6b272404504a36aaeab43
amodeose <amodeosa@gmail.com>
2022-09-13 21:16:07 -0400

    bind ref for each input

--- src/components/cart/OrderForm.js
+++ src/components/cart/OrderForm.js
@@ -1,21 +1,34 @@
 import classes from "./OrderForm.module.css";
 import Button from "../UI/Button";
+import { useRef, useState } from "react";
 
 const OrderForm = (props) => {
+  const [submitted, setSubmitted] = useState(false);
+
+  const firstNameRef = useRef();
+  const lastNameRef = useRef();
+  const addressRef = useRef();
+  const postalCodeRef = useRef();
+
+  const submitHandler = event => {
+    event.preventDefault();
+
+  }
+
   return (
-    <form className={classes.orderForm}>
+    <form onSubmit={submitHandler} className={classes.orderForm}>
       <h1>Details</h1>
       <label>First Name</label>
-      <input />
+      <input ref={firstNameRef}/>
       <label>Last Name</label>
-      <input />
+      <input ref={lastNameRef}/>
       <label>Address</label>
-      <input />
+      <input ref={addressRef}/>
       <label>Postal Code</label>
-      <input />
+      <input ref={postalCodeRef}/>
       <div className={classes.formControls}>
         <Button onClick={props.handleCancel}>Cancel</Button>
-        <Button>Submit</Button>
+        <Button type='submit'>Submit</Button>
       </div>
     </form>
   );
